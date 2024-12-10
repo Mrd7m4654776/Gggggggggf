c78fe9e4b73f00edcce8c99d67c1850850dbe809
Mrd7m4654776 <mrd7m888@gmail.com>
2024-12-10 07:52:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
-const play = require('play-dl'); 
-require('dotenv').config();
+const Discord = require("discord.js")
+const { Client, GatewayIntentBits, DiscordAPIError, ChannelType, CategoryChannel, PermissionsBitField, ApplicationCommandOptionType } = require('discord.js');
+const config = require("./config.json")
+const selecttest = require("./rules.json")
 
+const { REST, Routes } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ModalBuilder, MessageEmbed, MessageAttachment, StringSelectMenuBuilder, EmbedBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessageReactions
+  ],
 });
-const express = require("express")
-const app = express();
-var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
-app.get('/', (req, res) => {
-  res.send(`
-  <body>
-  <center><h1>Bot 24H ON!</h1></center
-  </body>`)
-});
-const channelId = '1210233988699131946';
-const guildId = '1011820688228765828'; 
-const Url = 'https://youtu.be/fLkdQeeRtYs?si=02zwuFdQbytcD09f'; 
-
+require('dotenv').config()
 client.on('ready', () => {
-  console.log(`✅ | Logged in as ${client.user.tag}`);
-  joinAndPlayQuran(guildId, channelId);
+  console.log(`Logged in as ${client.user.tag}!`);
+})
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(config.ID_Bot_Discord), {
+      body: [
+
+        {
+          name: config.cmdRules,
+          description: config.cmd_description,
+        },
+      ]
+    });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on('interactionCreate', async (interaction) => {
+  if (interaction.commandName === 'rules') {
+
+
+    await interaction.deferReply({ ephemeral: false }).catch((err) => null);
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId('rules')
+          .setPlaceholder(config.cmd_description)
+          .addOptions(selecttest.map(e => {
+            return {
+              ...e
+            }
+          })),
+      );
+    
+    await interaction.editReply({ content: config.cmdTitle, ephemeral: true, components: [row] })
+  }
+  if (interaction.isStringSelectMenu()) {
+    const choice = interaction.values[0];
+    const selectedElement = selecttest.find(element => choice === element.value);
+
+    if (selectedElement) {
+      await interaction.deferReply({ ephemeral: false });
+      await interaction.editReply({ content: `${selectedElement.emoji} ${selectedElement.label} \n ${selectedElement.message}`, ephemeral: true });
+    }
+  }
 });
-
-async function playQuran(channel, message) {
-  const player = createAudioPlayer();
-  const connection = joinVoiceChannel({
-    channelId: channel.id,
-    guildId: channel.guild.id,
-    adapterCreator: channel.guild.voiceAdapterCreator,
-  });
-
-  connection.subscribe(player);
-
-  const stream = await play.stream(Url);
-  const resource = createAudioResource(stream.stream, { inputType: stream.type });
-  player.play(resource);
-
-  player.on(AudioPlayerStatus.Playing, () => {
-    message.channel.send({ embeds: [new EmbedBuilder().setDescription('**تم تشغيل القرآن**').setColor(0x00ff00)] });
-  });
-
-  player.on('error', error => console.error(`Error: ${error.message}`));
-}
-
-async function joinAndPlayQuran(guildId, channelId) {
-  const channel = await client.channels.fetch(channelId);
-  const connection = joinVoiceChannel({
-    channelId: channel.id,
-    guildId: guildId,
-    adapterCreator: channel.guild.voiceAdapterCreator,
-  });
-
-  const player = createAudioPlayer();
-
-  const stream = await play.stream(Url);
-  const resource = createAudioResource(stream.stream, { inputType: stream.type });
-
-  player.play(resource);
-  connection.subscribe(player);
-
-  player.on(AudioPlayerStatus.Playing, () => console.log('Playing Quran'));
-  player.on('error', error => console.error(`Error: ${error.message}`));
-}
-
 client.login(process.env.TOKEN);
